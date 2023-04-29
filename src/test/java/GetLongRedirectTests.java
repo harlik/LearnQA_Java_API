0db90f678f452fe98fd85762fc9e9a5a1c49d35a
Olga Svalova <harlozavr@gmail.com>
2023-04-29 20:38:54 +0300

    Ex7 "long redirect": get all redirect URLs starting from the long_redirect endpoint.

--- src/test/java/GetLongRedirectTests.java
+++ src/test/java/GetLongRedirectTests.java
@@ -14,4 +14,23 @@ public class GetLongRedirectTests {
                 .andReturn();
         System.out.println(response.getHeader("Location"));
     }
+
+    @Test
+    public void countRedirectsTest() {
+        Response response;
+        String url = "https://playground.learnqa.ru/api/long_redirect";
+        int redirectsCounter = 0;
+        do {
+            response = given()
+                    .redirects()
+                    .follow(false)
+                    .get(url)
+                    .andReturn();
+            url = response.getHeader("Location");
+            redirectsCounter++;
+            System.out.println(url);
+        } while((response.getStatusCode() != 200) && (url != null));
+        redirectsCounter--;
+        System.out.printf("Number of redirects = %1$d\n", redirectsCounter);
+    }
 }
